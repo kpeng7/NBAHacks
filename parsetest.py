d75a64e58084ea9a7ef8e8d39a08f4fc214045a7
Jack Ye <jackye315@gmail.com>
2017-07-31 01:49:24 -0400

    making teams:

--- parsetest.py
+++ parsetest.py
@@ -1,8 +1,10 @@
 from openpyxl import load_workbook
+import p2
 
 wb = load_workbook('Analytics_Attachment.xlsx')
 
 print wb.get_sheet_names()
+opponents = {}
 ws = wb['Division_Info']
 for row in ws.rows:
-    print row[0].value
+    Team1 = Team(self, row[0].value, row[1].value, row[2].value, opponents):
