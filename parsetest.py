b1da9d56828f315c82a7fcecf4aaeba808989292
jaye <jaye@JAYE-US.us.oracle.com>
2017-08-02 12:06:12 -0400

    generate GAMEDATA

--- parsetest.py
+++ parsetest.py
@@ -6,9 +6,12 @@ wb = load_workbook('Analytics_Attachment.xlsx')
 print wb.get_sheet_names()
 opponents = {}
 ws = wb['Division_Info']
+games = wb['2016_17_NBA_Scores']
 team_list = {}
 division_list = {}
 conference_list = {}
+GAME_DATA = {}
+
 
 #generateTeams
 for row in ws.rows:
@@ -17,20 +20,18 @@ for row in ws.rows:
 print team_list.keys()
 
 #generateDivisions
-for row in ws.rows:
-    current_row = row[1].value
-    if not division_list.has_key(current_row):
+for team in team_list:
+    current_division = team.division
+    if not division_list.has_key(current_division):
         teams_division_list = []
-        for team in ws.rows:
-            if team[1].value == current_row:
-                new_team = Team(self, team[0].value, current_row, team[2].value, opponents):
-                teams_division_list.append(new_team)
-        division1 = Division(self, current_row, teams_division_list)
-        division_list[current_row] = division1
+        for team_temp in team_list:
+            if team_temp.division == current_division:
+                teams_division_list.append(team_temp)
+        division_temp = Division(self, current_division, teams_division_list)
+        division_list[current_division] = division1
 print division_list.keys()
 
 #generateConference
-current_conference = "West"
 teams_west_list = []
 teams_east_list = []
 for team in team_list:
@@ -43,3 +44,14 @@ east_conference = Conference(self, "East", teams_east_list)
 conference_list["West"] = west_conference
 conference_list["East"] = east_conference
 print conference_list.keys()
+
+#generateGame_Data
+for row in games.rows:
+    current_date = row[0].value
+    if not GAME_DATA.has_key(current_date):
+        games_list = []
+        for row_temp in games.rows:
+            game = (row[1].value, row[2].value, (row[3].value, row[4].value))
+            games_list.append(game)
+        GAME_DATA[current_date] = games_list
+print GAME_DATA.keys()
